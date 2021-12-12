649a711cf8dc16af8d6a91d3a04cc43d2e484ebb
java_ht <java_ht@163.com>
2021-12-12 22:56:56 +0800

    初始化

---
+++ src/main/java/com/zht/mapreduce/wordcount/WordCountMapper.java
@@ -0,0 +1,33 @@
+package com.zht.mapreduce.wordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+/*
+* KEYIN   map阶段输入的key类型            偏移量的类型LongWritable
+* VALUEIN  map阶段输入的value类型                   Text
+* KEYOUT    map阶段输出的key类型                    Text(单词类型)
+* VALUEOUT   map阶段输出的value类型               IntWritable(单词的个数)
+ *  */
+public class WordCountMapper extends Mapper<LongWritable, Text,Text, IntWritable> {
+
+   private  Text outK = new Text();
+   private IntWritable outV = new IntWritable(1);
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+         //获取一行
+        String[] words = value.toString().split(" ");
+
+        for (String word : words) {
+            //封装outK
+            outK.set(word);
+            //写出
+            context.write(outK,outV);
+        }
+    }
+}
---
+++ src/main/java/com/zht/mapreduce/wordcount/WordCountReduce.java
@@ -0,0 +1,31 @@
+package com.zht.mapreduce.wordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+
+/*
+*
+* KEYIN       reduce阶段输入的key类型        Text
+* VALUEIN       reduce阶段输入的value类型     Int
+* KEYOUT        reduce阶段输出的key类型       Text
+* VALUEOUT         reduce阶段输出的value类型   Int
+* */
+public class WordCountReduce extends Reducer<Text, IntWritable,Text, IntWritable> {
+    IntWritable outV = new IntWritable();
+    @Override
+    //都是输入的key 和values
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+        int sum = 0;
+
+        for (IntWritable value : values) {
+            sum +=value.get();
+        }
+        outV.set(sum);
+        context.write(key,outV);
+    }
+}
